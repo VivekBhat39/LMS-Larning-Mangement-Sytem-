1b308c5feb894f5a2ec393d96c5362d14003bace
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-11-03 15:22:10 +0530

    app.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,18 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
   return (
     <div>
-      
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link to="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link to="/courses">Courses</Nav.Link>
+            <Nav.Link to="/users">Users</Nav.Link>
+            <Nav.Link to="/changepassword">Change password</Nav.Link>
+            <Nav.Link to="/logout">Logout</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   )
 }
