2f94b6370d1bc01541771d5009ac3e994780d2d3
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-11-03 17:13:08 +0530

    qq

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Login = () => {
     return (
         <div>
-           </div>
+            
+        </div>
     )
 }
 
-export default Login
+export default Login;
