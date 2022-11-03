e15335bb299117fae7b9158a9e69f8898272c593
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-11-03 16:04:42 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Login from './components/Login';
 import Cards from './components/Cards'
 
 function App() {
   return (
     <div className="App">
-      < Cards />
+    <BrowserRouter>
+      <Header />
+      <Cards />
+      <Routes>
+
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
