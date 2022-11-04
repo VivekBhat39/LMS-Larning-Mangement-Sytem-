e9cf1d59896e7fb03cd424b9c286974dc56e4459
vky39 <vivek.bhat571@gmail.com>
2022-11-04 12:54:39 +0530

    Course Add Button Added

--- src/components/courses/CourseTable.jsx
+++ src/components/courses/CourseTable.jsx
@@ -1,26 +1,25 @@
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-import { useEffect, useState } from 'react';
-import axios from 'axios'
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function CourseTable() {
-  
   let [course, setCourse] = useState([]);
 
-  useEffect(()=>{
-    axios.get("http://localhost:8081/trainer/course/6364ab916d947aeffe2204b4").then(
-      (response)=>{
-        console.log(response.data)
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/trainer/course/6364ab916d947aeffe2204b4")
+      .then((response) => {
+        console.log(response.data);
         setCourse(response.data.data);
-      }
-    )
-  })
+      });
+  });
 
   return (
     <Container>
       <br />
       <Table striped bordered hover>
-        <thead className='bg-primary'>
+        <thead className="bg-primary">
           <tr>
             <th>Sr.No.</th>
             <th colSpan={2}>Action</th>
@@ -34,24 +33,30 @@ function CourseTable() {
           </tr>
         </thead>
         <tbody>
-          {
-            course.map((course, i)=>{
-              return(
-                <tr key={i}>
-                  <td class="font-weight-bold">{i + 1}</td>
-                  <td><button type="button" class="btn btn-primary"><i class="fa-sharp fa-solid fa-pen"></i></button></td>
-                  <td><button type="button" class="btn btn-primary"><i class="fa-solid fa-trash"></i></button></td>
-                  <td>{course.trainerid}</td>
-                  <td>{course.title}</td>
-                  <td>{course.description}</td>
-                  <td>{course.imagepath}</td>
-                  <td>{course.mrp}</td>
-                  <td>{course.price}</td>
-                  <td>{course.status}</td>
-                </tr>
-              )
-            })
-          }
+          {course.map((course, i) => {
+            return (
+              <tr key={i}>
+                <td class="font-weight-bold">{i + 1}</td>
+                <td>
+                  <button type="button" class="btn btn-primary">
+                    <i class="fa-sharp fa-solid fa-pen"></i>
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-primary">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+                <td>{course.trainerid}</td>
+                <td>{course.title}</td>
+                <td>{course.description}</td>
+                <td>{course.imagepath}</td>
+                <td>{course.mrp}</td>
+                <td>{course.price}</td>
+                <td>{course.status}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </Container>
