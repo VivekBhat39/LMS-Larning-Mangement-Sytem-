9fe82ea244358622579adc6efbad8ffec9b69a31
vky39 <vivek.bhat571@gmail.com>
2022-11-04 11:22:15 +0530

    Course Table update

--- src/components/courses/CourseTable.jsx
+++ src/components/courses/CourseTable.jsx
@@ -1,36 +1,80 @@
 import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function CourseTable() {
+  
+  let [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    axios.get("https://reqres.in/api/users").then(
+      (response)=>{
+        console.log(response.data)
+        setUsers(response.data.data);
+      }
+    )
+  })
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+    <Container>
+      <Table className="Container fluid" striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <br />
+      <Table striped bordered hover>
+        <thead className='bg-primary'>
+          <tr>
+            <th>No</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user, i)=>{
+              return(
+                <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td>{user.avatar}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
